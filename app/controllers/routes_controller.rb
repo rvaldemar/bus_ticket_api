454effe925eae266a423053f67cc516dea2c8a36
Rui Santos <rvaldemar93@gmail.com>
2021-08-29 13:10:54 -0300

    PathFinder improvement

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,3 +1,17 @@
 class RoutesController < ApplicationController
+  def search
+    attrs = search_params
+    attrs[end_date] = params[:end_date] ?
+      params[:end_date] || search_params[:start_date].to_datetime.at_end_of_day
 
+    Route.where(start_point_params)
+  end
+
+  private
+  def start_point_params
+    params.permit(
+      :start_date,
+      :start_id,
+    )
+  end
 end
--- app/services/path_finder.rb
+++ app/services/path_finder.rb
@@ -1,52 +1,15 @@
 class PathFinder
-  def initialize(vertices)
-    # No. of vertices
-    @v = vertices
-
-    # default array to store graph
-    @graph = {}
-    (0...@v).each { |i| @graph[i] = []}
-
+  def initialize(options)
+    @s = options[:start_id]
+    @d = options[:destination_id]
+    @start_date = options[:start_date]
+    @end_date = options[:end_date]
     @result = []
   end
 
-  # function to add an edge to graph
-  def addEdge(u, v)
-    @graph[u].push(v)
-  end
-
-    '''A recursive function to print all paths from u to d.
-    visited[] keeps track of vertices in current path.
-    path[] stores actual vertices and path_index is current
-    index in path[]'''
-    def printAllPathsUtil(u, d, visited, path)
-
-      # Mark the current node as visited and store in path
-      visited[u]= true
-      path.push(u)
-
-      # If current vertex is same as destination, then print
-      # current path[]
-      if u == d
-          @result.push path.clone
-      else
-        # If current vertex is not destination
-        # Recur for all the vertices adjacent to this vertex
-        @graph[u].each do |i|
-          unless visited[i]
-              printAllPathsUtil(i, d, visited, path)
-          end
-        end
-      end
-
-      # Remove current vertex from path[] and mark it as unvisited
-      path.pop
-      visited[u]= false
-    end
-
-
   # Prints all paths from 's' to 'd'
-  def printAllPaths(s, d)
+  def getAllPaths
+    'Calculating paths...'
     # Mark all the vertices as not visited
     visited = {}
 
@@ -54,8 +17,45 @@ class PathFinder
     path = []
 
     # Call the recursive helper function to print all paths
-    printAllPathsUtil(s, d, visited, path)
+    printAllPathsUtil(@s, visited, path, @start_date)
     @result
+  end
+
+  private
+
+  '''A recursive function to print all paths from u to d.
+  visited[] keeps track of vertices in current path.
+  path[] stores actual vertices and path_index is current
+  index in path[]'''
+  def printAllPathsUtil(u, visited, path, start_date)
+
+    # Mark the current node as visited and store in path
+    visited[u]= true
+    path.push(u)
+
+    # If current vertex is same as destination, then print
+    # current path[]
+    if u == @d
+        @result.push path.clone
+    else
+      # If current vertex is not destination
+      # Recur for all the vertices adjacent to this vertex
+
+      Route.where(start_id: u, start_date: start_date..@end_date, end_date: start_date..@end_date).each do |route|
+        unless visited[route.destination_id]
+            printAllPathsUtil(route.destination_id, visited, path, route.start_date)
+        end
+      end
+    end
+
+    # Remove current vertex from path[] and mark it as unvisited
+    path.pop
+    visited[u]= false
+  end
+
+  # function to add an edge to graph
+  def addEdge(u, v)
+    @graph[u].push(v)
   end
 
 end
